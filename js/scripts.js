a7ecee8cc41991f0c90858be57eecee99636197e
icarly <carlymarie.07@gmail.com>
2020-10-29 17:18:42 -0400

    add in hot link image alt tags

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 	var $grid = $('.grid').masonry({
 	  itemSelector: '.grid-item',
 	  percentPosition: true,
-	  columnWidth: '.grid-sizer'
+	  columnWidth: '.grid-sizer',
 	});
 	// layout Masonry after each image loads
 	$grid.imagesLoaded().progress( function() {
